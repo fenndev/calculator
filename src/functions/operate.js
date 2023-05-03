24e5f532d95e1bec5070d07c108eed619dd11a0b
fenndev <fenndev@proton.me>
2023-05-02 23:35:54 -0700

    Removed unncessary console.log

--- src/functions/operate.js
+++ src/functions/operate.js
@@ -12,7 +12,6 @@ export default function operate(operator, firstNum, secondNum) {
             break;
         case 'divide':
             result = secondNum === 0 ? null : firstNum / secondNum;
-            console.log('secondNum was null!');
             break;
         default:
             result = null;
