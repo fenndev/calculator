102dc0f323768ee660d3aedcba23dd50a1a64c05
fenndev <fenndev@proton.me>
2023-05-02 23:34:47 -0700

    Fixed decimal handling

--- src/functions/concatenateNumber.js
+++ src/functions/concatenateNumber.js
@@ -1,3 +1,4 @@
 export default function concatenateNumber(current, newDigit) {
-    return current * 10 + Number.parseFloat(newDigit);
+    if (!current.toString().includes('.')) return current * 10 + Number.parseFloat(newDigit);
+    else return Number.parseFloat(current + newDigit);
 }
