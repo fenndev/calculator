38832928d70be930466db6a169bc08d313546da7
fenndev <fenndev@proton.me>
2023-05-08 01:29:51 -0700

    Refactored code & adjusted 'backspace' function

--- src/stores/calculator.js
+++ src/stores/calculator.js
@@ -1,6 +1,5 @@
-import { createEffect, createSignal, onCleanup } from 'solid-js';
+import { createEffect, createSignal } from 'solid-js';
 import operate from '../functions/operate';
-import getOperatorSymbol from '../functions/getOperatorSymbol';
 import concatenateNumber from '../functions/concatenateNumber';
 
 export default function Calculator() {
@@ -41,54 +40,67 @@ export default function Calculator() {
         if (currentNum == null) return;
         switch (operator) {
             case 'decimal':
-                if (currentNum() == null) {
-                    setCurrentNum('0.');
-                } else if (!currentNum().toString().includes('.')) {
-                    setCurrentNum(currentNum() + '.');
-                }
+                handleDecimal(currentNum, setCurrentNum);
                 break;
             case 'equals':
-                if (
-                    previousNum() !== null &&
-                    currentNum() !== null &&
-                    operatorSelected() !== null
-                ) {
-                    setCurrentNum(
-                        operate(
-                            operatorSelected(),
-                            Number.parseFloat(previousNum()),
-                            Number.parseFloat(currentNum())
-                        )
-                    );
-                    setOperatorSelected(null);
-                    setPreviousNum(null);
-                }
+                handleEquals();
                 break;
             case 'clear':
                 clear();
                 break;
             case 'backspace':
-                let currentNumString = Number.parseFloat(currentNum().toString().slice(0, -1));
-                if (!currentNumString) setCurrentNum(0);
-                else setCurrentNum(Number.parseFloat(currentNumString));
+                handleBackspace();
                 break;
             default:
-                if (operator) {
-                    if (operatorSelected() && !previousNum()) {
-                        setOperatorSelected(operator);
-                    } else if (operatorSelected() && previousNum()) {
-                        setOperatorSelected(operator);
-                        setCurrentNum(operate(operatorSelected(), previousNum(), currentNum()));
-                        setPreviousNum(currentNum());
-                    } else {
-                        setPreviousNum(currentNum());
-                        setOperatorSelected(operator);
-                    }
-                    setNewEntry(true);
-                }
+                handleOperation(operator);
                 break;
         }
     };
+
+    function handleOperation(operator) {
+        if (operator) {
+            if (operatorSelected() && !previousNum()) {
+                setOperatorSelected(operator);
+            } else if (operatorSelected() && previousNum()) {
+                setOperatorSelected(operator);
+                setCurrentNum(operate(operatorSelected(), previousNum(), currentNum()));
+                setPreviousNum(currentNum());
+            } else {
+                setPreviousNum(currentNum());
+                setOperatorSelected(operator);
+            }
+            setNewEntry(true);
+        }
+    }
+
+    function handleBackspace() {
+        let currentNumString = Number.parseFloat(currentNum().toString().slice(0, -1));
+        if (!currentNumString || newEntry()) setCurrentNum(0);
+        else setCurrentNum(Number.parseFloat(currentNumString));
+    }
+
+    function handleDecimal() {
+        if (currentNum() == null) {
+            setCurrentNum('0.');
+        } else if (!currentNum().toString().includes('.')) {
+            setCurrentNum(currentNum() + '.');
+        }
+    }
+
+    function handleEquals() {
+        if (previousNum() !== null && currentNum() !== null && operatorSelected() !== null) {
+            setCurrentNum(
+                operate(
+                    operatorSelected(),
+                    Number.parseFloat(previousNum()),
+                    Number.parseFloat(currentNum())
+                )
+            );
+            setOperatorSelected(null);
+            setPreviousNum(null);
+            setNewEntry(true);
+        }
+    }
 
     return {
         clear,
